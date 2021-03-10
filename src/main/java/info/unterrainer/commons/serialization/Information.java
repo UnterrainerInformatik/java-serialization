d9d970855586d5b1a2c72ab295a686883fbf3d3c
Psilo <psilo@unterrainer.info>
2021-03-10 09:19:04 +0100

    repair information mechanism and bump version

--- src/main/java/info/unterrainer/commons/serialization/Information.java
+++ src/main/java/info/unterrainer/commons/serialization/Information.java
@@ -2,6 +2,6 @@ package info.unterrainer.commons.serialization;
 
 public class Information {
 	public static final String name = "Serialization";
-	public static final String buildTime = "2021-03-10T08:00:05Z";
-	public static final String pomVersion = "0.1.1";
+	public static final String buildTime = "2021-03-10T08:18:44Z";
+	public static final String pomVersion = "0.1.2";
 }
